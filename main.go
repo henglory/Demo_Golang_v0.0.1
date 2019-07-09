3e612aa59968b467bff55cfc76b55a455e9aecf1
Nuttapong <nuttapong@Nuttapongs-MacBook-Pro.local>
2019-07-09 10:19:54 +0700

    add server

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/henglory/Demo_Golang_v0.0.1/server"
+	"github.com/henglory/Demo_Golang_v0.0.1/service"
+)
+
+func main() {
+
+	s := service.Service{}
+
+	log.Println("Start server")
+	serv := server.NewServer(s)
+	serv.Start()
+	defer serv.Close()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Server exiting")
+}
---
+++ server/server.go
@@ -0,0 +1,65 @@
+package server
+
+import (
+	"context"
+	golog "log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/henglory/Demo_Golang_v0.0.1/service"
+)
+
+type errorResponse struct {
+	ResponseCode int64  `json:"responseCode"`
+	Reason       string `json:"reason"`
+	RawRequest   string `json:"rawRequest"`
+}
+
+type Server struct {
+	srv *http.Server
+	s   service.Service
+}
+
+func NewServer(s service.Service) *Server {
+	server := &Server{
+		s: s,
+	}
+	return server
+}
+
+func (server *Server) Start() {
+	go server.ginStart()
+}
+
+func (server *Server) Close() {
+	server.srv.Close()
+}
+
+type readiness struct {
+	Success bool `json:"success"`
+}
+
+func (server Server) ginStart() {
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+
+	r.GET("/readiness", func(c *gin.Context) {
+		c.JSON(200, readiness{Success: true})
+	})
+
+	server.srv = &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	if err := server.srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		golog.Fatalf("listen: %s\n", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	if err := server.srv.Shutdown(ctx); err != nil {
+		golog.Fatal("Server Shutdown:", err)
+	}
+	defer cancel()
+}
